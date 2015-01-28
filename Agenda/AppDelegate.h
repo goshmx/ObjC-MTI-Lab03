6382b2f386a73e5fe34342a09d76e3152c4f18b5
Gosh <osvaldocube@gmail.com>
2015-01-28 02:02:35 -0600

    Initial Commit

---
+++ Agenda/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Agenda
+//
+//  Created by TRON on 28/01/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Agenda/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Agenda
+//
+//  Created by TRON on 28/01/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
